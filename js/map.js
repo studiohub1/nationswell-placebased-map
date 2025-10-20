440427a17d769c8c676b6aa61a9f7c90e5690b18
Kristin Baumann <hello@kristin-baumann.com>
2025-10-20 10:50:48 +0200

    Adjusted population layer image (removed northern line)

--- js/map.js
+++ js/map.js
@@ -400,7 +400,7 @@ export function Map({ usGeoData, places, partners, allFocusAreas }) {
         })}
       </svg>
       <img
-        src="${REPO_URL}/data/population-density-layer-6x_adjusted_compressed.png"
+        src="${REPO_URL}/data/population-density-layer-6x_adjusted_without_northern_line_compressed.png"
         class="map-overlay absolute top-0 left-0 w-full h-full object-contain block pointer-events-none opacity-90 object-contain "
         alt="Population density overlay"
         width="${width}"
